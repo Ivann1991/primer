dd93660e2c1762f8e388d4d50ab6400dd0dd2821
Ivan <Korneychik91@yandex.ru>
2022-10-01 21:54:51 +0300

    examples random

--- examples_000/Program.cs
+++ examples_000/Program.cs
@@ -1,4 +1,6 @@
-﻿int nubmerA = 3;
-int numberB = 4;
+﻿int nubmerA = new Random().Next (1,10);
+Console.WriteLine (nubmerA);
+int numberB = new Random ().Next (1,50);
+Console.WriteLine (numberB);
 int result = nubmerA + numberB;
 Console.WriteLine (result);
