c7918975c7526e275c413402d19ecd0264e99425
Ivan <Korneychik91@yandex.ru>
2022-10-01 22:10:23 +0300

    examples_002

---
+++ examples_002/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введи имя епта");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "иван")
+{
+     Console.WriteLine ("Здарова ИВАНЫЧ");
+} 
+else 
+{
+    Console.Write ("пшшол вон ");
+    Console.WriteLine (username);
+
+}
